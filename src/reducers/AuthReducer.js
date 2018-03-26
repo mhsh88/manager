953f76e9d05cd33ee85779e6c164a6dda97e05f6
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-26 22:17:12 +0000

    complete login

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -25,13 +25,7 @@ export default (state = INITIAL_STATE, action) => {
        loading: true,
        error: '' };
     case LOGIN_USER_SUCCESS:
-      return { ...state,
-         user: action.payload,
-         error: '',
-         loading: false,
-         email: '',
-         password: ''
-       };
+      return { ...state, ...INITIAL_STATE, user: action.payload };
     case LOGIN_USER_FAIL:
       return { ...state, error: 'Authentication Failed.', password: '', loading: false };
     default:
