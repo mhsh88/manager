481c67b66a25f040e3b0be1402774d0630eb91d0
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-26 10:13:20 +0000

    create AuthReducer.js for handling user input for email

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
   render() {
     return (
       <Card>
@@ -9,6 +14,7 @@ class LoginForm extends Component {
           <Input
             lable="Email"
             placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
           />
         </CardSection>
         <CardSection>
@@ -29,4 +35,5 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+
+export default connect(null, { emailChanged })(LoginForm);
