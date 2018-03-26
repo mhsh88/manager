8e78c049cacfb9034feb6227e1a40eb1fea21cda
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-26 23:25:55 +0000

    add react-native-router-flux and create Router and add scene

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 
 class App extends Component {
@@ -24,7 +23,7 @@ firebase.initializeApp(config);
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
