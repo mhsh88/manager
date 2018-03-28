731f252dea997bae843eaaa2ca946a1aba45c8a8
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-28 11:55:51 +0000

    create employee reducer and connect to EmployeeCreate.js via mapStateToProps

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
@@ -9,6 +11,8 @@ class EmployeeCreate extends Component {
           <Input
             lable="Name"
             placeholder="Jane"
+            value={this.props.name}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
           />
         </CardSection>
 
@@ -16,6 +20,8 @@ class EmployeeCreate extends Component {
           <Input
             lable="Phone"
             placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
           />
         </CardSection>
 
@@ -29,4 +35,9 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeFrom;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
