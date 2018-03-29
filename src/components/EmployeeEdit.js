769fa2b9e7f6e194f9685fe3667af4e4a555e7a0
hossein sharifi <hosseinsharifi@hotmail.com>
2018-03-29 13:40:50 +0000

    create action to delete employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -22,6 +22,14 @@ class EmployeeEdit extends Component {
     const { phone, shift } = this.props;
     Communications.text(phone, `Your upcomming shift is on ${shift}`);
   }
+
+  onAccept() {
+
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
   render() {
     return (
       <Card>
@@ -45,6 +53,8 @@ class EmployeeEdit extends Component {
 
         <Confirm
           visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
         >
           Are you sure you want to delete this?
         </Confirm>
